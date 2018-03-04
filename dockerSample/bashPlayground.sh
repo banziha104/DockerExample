e46040b635478aef370747554123b202907d3016
iyeongjun <they6687@naver.com>
2018-03-04 20:00:33 +0900

    add Install & Usage

--- dockerSample/bashPlayground.sh
+++ dockerSample/bashPlayground.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-echo "test" >
+echo "test"
+hello="hello"
+echo $hello
+temp=50
+if [ $temp -le 100 ]; then
+    echo $temp
+fi
 
+for i in {1..100}
+do
+if [ $i -le 50 ]; then
+    echo "$i is less Than 50"
+fi
+done
